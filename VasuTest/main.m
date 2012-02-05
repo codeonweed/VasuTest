fdadb5501e7ef897b54df8383a276e531e6253b3
Shrinivas Gadde <Shrinivas.Gadde@gmail.com>
2012-02-05 09:56:58 -0500

    Initial Commit

---
+++ VasuTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VasuTest
+//
+//  Created by Shrinivas Gadde on 2/5/12.
+//  Copyright (c) 2012 HCL AXON. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
